10255e78fc4edcb5389acb9f6d86885480e930d1
MariaPilarPerez <mdpperezgy@fpvirtualaragon.es>
2025-11-13 12:16:25 +0100

    Initial commit 13 de noviembre

---
+++ app/src/main/java/com/campusdigitalfp/habitossaludables/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.campusdigitalfp.habitossaludables
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.campusdigitalfp.habitossaludables.ui.theme.HabitosSaludablesTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HabitosSaludablesTheme {
+                VistaHabito(Habito("Comer Saludable","Objetivo de comer saludable todos los dias de la semana"))
+            }
+        }
+    }
+}
+data class Habito (val titulo: String, val descripcion: String)
+
+//Esta es la función principal, en la que organizamos todos los elementos.
+@Composable
+fun VistaHabito(Habito: Habito) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.estilo_de_vida),
+            contentDescription = "Icono estilo de vida",
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+                .border(width = 1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+// Mantenemos un registro de si el mensaje está expandido o no
+        var isExpanded by remember {mutableStateOf(false)}
+        //Cambiamos el estado de la variable isExpanded cuando hacemos clic en esta columna
+        Column (modifier = Modifier.clickable {isExpanded = !isExpanded}){
+            Text(
+                text = Habito.titulo,
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                    shape = MaterialTheme.shapes.medium
+                ) {
+                    Text(
+                        text = Habito.descripcion,
+                        modifier = Modifier.padding(4.dp),
+    // Si el mensaje está expandido mostramos
+    // todo el contenido, sino solo la primera linea
+                        maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+
+    }
+
+@Composable
+fun VistaListaHabitos(habitos: List<Habito>){
+    LazyColumn {
+        items(habitos){
+            habito->VistaHabito(habito)
+        }
+    }
+}
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES,
+showBackground = true, name="Dark Mode"
+    )
+@Preview(showBackground = true)
+@Composable
+fun PreviewVistaHabito() {
+    HabitosSaludablesTheme {
+      VistaListaHabitos(SampleData.habitSample)
+    }
+
+}
