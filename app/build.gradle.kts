c704c4bbb2627ce44b73fece3122d09b8b25ccd2
MariaPilarPerez <mdpperezgy@fpvirtualaragon.es>
2026-02-09 13:07:36 +0100

    Agregado modificaciones para firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -66,5 +67,7 @@ dependencies {
     implementation("androidx.compose.runtime:runtime:1.10.0")
     implementation("androidx.compose.runtime:runtime-livedata:1.10.0")
     implementation("androidx.compose.runtime:runtime-rxjava2:1.10.0")
+    implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
 }
